4e73c960db2db66ff91b88860f569356ec261ae4
Fabian Meyer <user8324@posteo.net>
2019-01-08 16:37:17 +0100

    Fix pso optimization

--- include/psocpp.h
+++ include/psocpp.h
@@ -13,32 +13,29 @@
 #include <limits>
 #include <functional>
 #include <iostream>
+#include <chrono>
+#include <iomanip>
 
 namespace pso
 {
     template<typename Scalar>
-    class PSO
+    class Optimizer
     {
     public:
         typedef Eigen::Matrix<Scalar, Eigen::Dynamic, Eigen::Dynamic> Matrix;
         typedef Eigen::Matrix<Scalar, Eigen::Dynamic, 1> Vector;
         typedef std::function<Scalar(const Vector &)> Objective;
+        typedef typename Matrix::Index Index;
+
+        struct Result
+        {
+            size_t iterations;
+            bool converged;
+            Scalar fval;
+            Vector xval;
+        };
 
     private:
-
-        Objective objective_;
-        Matrix::Index best_;
-
-        bool particlesInitialized_;
-        Matrix particles_;
-        Matrix particlesPrev_;
-
-        Vector funcvalues_;
-
-        Matrix velocities_;
-
-        Matrix bounds_;
-
         size_t threads_;
 
         size_t maxit_;
@@ -51,124 +48,176 @@ namespace pso
 
         bool verbose_;
 
-        std::function<Scalar()> dice_;
-
-        void initParticles()
+        std::function<Scalar()> getDice() const
         {
-            for(Matrix::Index i = 0; i < particles_.cols(); ++i)
-            {
-                for(Matrix::Index j = 0; j < particles_.rows(); ++j)
-                {
-                    Scalar min = bounds(0, j);
-                    Scalar max = bounds(0, j);
-                    Scalar diff = max - min;
-                    particles_(j, i) = min + (dice_() * diff);
-                }
-            }
+            std::default_random_engine gen(std::time(0));
+            std::uniform_real_distribution<Scalar> distrib(0.0, 1.0);
+            return std::bind(distrib, gen);
         }
 
-        void initVelocities()
+        void randomizeParticles(const Matrix &bounds, Matrix &particles) const
         {
-            for(Matrix::Index i = 0; i < velocities_.cols(); ++i)
+            auto dice = getDice();
+            for(Index i = 0; i < particles.cols(); ++i)
             {
-                for(Matrix::Index j = 0; j < velocities_.rows(); ++j)
-                {
-                    Scalar min = bounds(0, j);
-                    Scalar max = bounds(0, j);
-                    Scalar diff = max - min;
-                    velocities_(j, i) = -diff + (dice_() * 2 * diff);
-                }
-            }
-        }
-
-        void evaluateObjective()
-        {
-            #pragma omp parallel num_threads(threads == 0 ? omp_get_max_threads() : threads_)
-            for(Matrix::Index i = 0; i < particles_.cols(); ++i)
-            {
-                Scalar tmp = objective_(particles_.col(i));
-                funcvalues_(i) = tmp < funcvalues_(i) ? tmp : funcvalues_(i);
-            }
-            funcvalues_.minCoeff(&best_);
-        }
-
-        void evaluateVelocities()
-        {
-            for(Matrix::Index i = 0; i < velocities_.cols(); ++i)
-            {
-                for(Matrix::Index j = 0; j < velocities_.rows(); ++j)
-                {
-                    Scalar velp = dice_() * (particlesPrev_(j, i) - particles_(j, i));
-                    Scalar velg = dice_() * (particles_(j, best_) - particles_(j, i));
-                    velocities_(j, i) = omega_ * velocities_(j, i) + phip_ * velp + phig_ * velg;
-                }
-            }
-        }
-
-        void maintainBounds()
-        {
-            for(Matrix::Index i = 0; i < particles_.cols(); ++i)
-            {
-                for(Matrix::Index j = 0; j < particles_.rows(); ++j)
+                for(Index j = 0; j < particles.rows(); ++j)
                 {
                     Scalar min = bounds(0, j);
                     Scalar max = bounds(1, j);
-                    Scalar val = particles_(j, i);
-                    particles_(j, i) = val < min ? min : val;
-                    particles_(j, i) = val > max ? max : val;
+                    Scalar diff = max - min;
+                    particles(j, i) = min + (dice() * diff);
                 }
             }
+        }
+
+        void randomizeVelocities(const Matrix &bounds, Matrix &velocities) const
+        {
+            auto dice = getDice();
+            for(Index i = 0; i < velocities.cols(); ++i)
+            {
+                for(Index j = 0; j < velocities.rows(); ++j)
+                {
+                    Scalar min = bounds(0, j);
+                    Scalar max = bounds(1, j);
+                    Scalar diff = max - min;
+                    velocities(j, i) = -diff + (dice() * 2 * diff);
+                }
+            }
+        }
+
+        void evaluateObjective(const Objective &objective,
+            const Matrix &particles,
+            Vector &fvals) const
+        {
+            #pragma omp parallel num_threads(threads == 0 ? omp_get_max_threads() : threads_)
+            for(Index i = 0; i < particles.cols(); ++i)
+                fvals(i) = objective(particles.col(i));
+        }
+
+        void maintainBounds(const Matrix &bounds, Matrix &particles) const
+        {
+            for(Index i = 0; i < particles.cols(); ++i)
+            {
+                for(Index j = 0; j < particles.rows(); ++j)
+                {
+                    Scalar min = bounds(0, j);
+                    Scalar max = bounds(1, j);
+                    Scalar val = particles(j, i);
+                    particles(j, i) = val < min ? min : val;
+                    particles(j, i) = val > max ? max : val;
+                }
+            }
+        }
+
+        void calculateVelocities(const Matrix &currParticles,
+            const Matrix &prevParticles,
+            const Index currBest,
+            Matrix &velocities) const
+        {
+            assert(velocities.rows() == currParticles.rows());
+            assert(velocities.cols() == currParticles.cols());
+            assert(velocities.rows() == prevParticles.rows());
+            assert(velocities.cols() == prevParticles.cols());
+            assert(currBest < currParticles.cols());
+
+            auto dice = getDice();
+            for(Index i = 0; i < velocities.cols(); ++i)
+            {
+                for(Index j = 0; j < velocities.rows(); ++j)
+                {
+                    Scalar velp = dice() * (prevParticles(j, i) - currParticles(j, i));
+                    Scalar velg = dice() * (currParticles(j, currBest) - currParticles(j, i));
+                    velocities(j, i) = omega_ * velocities(j, i) + phip_ * velp + phig_ * velg;
+                }
+            }
+        }
+
+        Result _minimize(const Objective &objective,
+            const Matrix &bounds,
+            Matrix &currParticles) const
+        {
+            Matrix prevParticles(currParticles.rows(), currParticles.cols());
+            Matrix velocities(currParticles.rows(), currParticles.cols());
+            Vector currFvals(currParticles.cols());
+            Vector prevFvals(currParticles.cols());
+            Index currBest = 0;
+            Index prevBest = 0;
+
+            // initialize velocities randomly
+            randomizeVelocities(bounds, velocities);
+
+            // evaluate objective function for the initial particles
+            evaluateObjective(objective, currParticles, currFvals);
+            currFvals.minCoeff(&currBest);
+
+            // init stop conditions
+            size_t it = 0;
+            Scalar fdiff = std::numeric_limits<Scalar>::infinity();
+            Scalar xdiff = std::numeric_limits<Scalar>::infinity();
+
+            while(fdiff > feps_ && xdiff > xeps_ && (maxit_ == 0 || it < maxit_))
+            {
+                prevParticles = currParticles;
+                prevFvals = currFvals;
+                prevBest = currBest;
+
+                // move particles by velocity and stay within bounds
+                currParticles += velocities;
+                maintainBounds(bounds, currParticles);
+
+                // evaluate objective for moved particles
+                evaluateObjective(objective, currParticles, currFvals);
+                for(Index i = 0; i < currFvals.size(); ++i)
+                {
+                    // if there was no improvement revert to old value
+                    if(currFvals(i) >= prevFvals(i))
+                    {
+                        currFvals(i) = prevFvals(i);
+                        currParticles.col(i) = prevParticles.col(i);
+                    }
+                }
+                currFvals.minCoeff(&currBest);
+
+                // calculate new velocities
+                calculateVelocities(currParticles, prevParticles, currBest, velocities);
+
+                // check if there was any improvement
+                if(currFvals(currBest) < prevFvals(prevBest))
+                {
+                    xdiff = (currParticles.col(currBest) -
+                        prevParticles.col(prevBest)).norm();
+                    fdiff = std::abs(currFvals(currBest) - prevFvals(prevBest));
+                }
+
+                if(verbose_)
+                {
+                    std::cout << "it=" << it
+                        << std::fixed << std::showpoint << std::setprecision(6)
+                        << "\tfdiff=" <<  fdiff
+                        << "\txdiff=" << xdiff
+                        << "\tf=" << currFvals(currBest)
+                        << "\tx=" << currParticles.col(currBest).transpose()
+                        << std::endl;
+                }
+
+                ++it;
+            }
+
+            Result result;
+            result.iterations = it;
+            result.converged = fdiff <= feps_ || xdiff <= xeps_;
+            result.fval = currFvals(currBest);
+            result.xval = currParticles.col(currBest);
+
+            return result;
         }
 
     public:
 
-        PSO()
-            : objective_(), best_(0), particlesInitialized_(false), particles_(),
-            particlesPrev_(),
-            funcvalues_(), velocities_(), bounds_(), threads_(1), maxit_(0),
-            xeps_(1e-6), feps_(1e-6), omega_(0.5), phip_(0.5), phig_(0.5),
-            verbose_(false), dice_()
+        Optimizer()
+            : threads_(1), maxit_(0), xeps_(1e-6), feps_(1e-6), omega_(0.5),
+            phip_(0.5), phig_(0.5), verbose_(false)
         {
-            std::default_random_engine gen(std::time(0));
-            std::uniform_real_distribution<Scalar> distrib(0.0, 1.0);
-            dice_ = std::bind(distrib, gen);
-        }
-
-        void setObjective(const Objective &objective)
-        {
-            objective_ = objective;
-        }
-
-        void setBounds(const Matrix &bounds)
-        {
-            if(bounds.rows() != 2)
-                throw std::runtime_error("bounds has not exactly 2 rows (min, max)");
-
-            for(Matrix::Index i = 0; i < bounds.cols(); ++i)
-            {
-                if(bounds(0, i) >= bounds(1, i))
-                    throw std::runtime_error("bounds min is greater than max");
-            }
-
-            bounds_ = bounds;
-        }
-
-        void setParticleCount(const size_t cnt, const size_t dim)
-        {
-            particles_.resize(dim, cnt);
-            particlesPrev_.resize(dim, cnt);
-            velocities_.resize(dim, cnt);
-            funcvalues_.resize(cnt);
-            particlesInitialized_ = false;
-        }
-
-        void setParticles(const Matrix &particles)
-        {
-            particles_ = particles;
-            particlesPrev_.resize(particles_.rows(), particles_.cols());
-            velocities_.resize(particles_.rows(), particles_.cols());
-            funcvalues_.resize(particles.cols());
-            particlesInitialized_ = true;
         }
 
         void setThreads(const size_t threads)
@@ -211,64 +260,42 @@ namespace pso
             verbose_ = verbose;
         }
 
-        void minimize()
+        Result minimize(const Objective &objective,
+            const Matrix &bounds,
+            const size_t particleCnt) const
         {
-            assert(particles_.rows() == velocities_.rows());
-            assert(particles_.cols() == velocities_.cols());
-
-            if(bounds.cols() != particles_.rows())
+            if(bounds.rows() != 2)
+                throw std::runtime_error("bounds has not exactly 2 rows (min, max)");
+            for(Index i = 0; i < bounds.cols(); ++i)
             {
+                if(bounds(0, i) >= bounds(1, i))
+                    throw std::runtime_error("bounds min is greater than max");
+            }
+
+            Matrix currParticles(bounds.cols(), particleCnt);
+            randomizeParticles(bounds, currParticles);
+
+            return _minimize(objective, bounds, currParticles);
+        }
+
+        Result minimize(const Objective &objective,
+            const Matrix &bounds,
+            const Matrix &particles) const
+        {
+            if(bounds.rows() != 2)
+                throw std::runtime_error("bounds has not exactly 2 rows (min, max)");
+            if(bounds.cols() != particles.rows())
                 throw std::runtime_error("columns of bounds and rows of "
                     "particles do not match");
-            }
-
-            // initialize particles if not initialized yet
-            if(!particlesInitialized_)
-                initParticles();
-            // initialize velocities randomly
-            initVelocities();
-
-            // evaluate objective function for the initial particles
-            funcvalues_ = std::numeric_limits<Scalar>::max();
-            evaluateObjective();
-
-            // init stop conditions
-            size_t it = 0;
-            Scalar fdiff = std::numeric_limits<Scalar>::max();
-            Scalar xdiff = std::numeric_limits<Scalar>::max();
-
-            while(fdiff > feps_ && xdiff > xeps_ && (maxit_ == 0 || it < maxit_))
+            for(Index i = 0; i < bounds.cols(); ++i)
             {
-                // evaoluate velocities
-                evaluateVelocities();
-                // move particles by velocity and stay within bounds
-                particlesPrev_ = particles_;
-                particles_ += velocities_;
-                maintainBounds();
-                // evaluate objective for moved particles
-                Matrix::Index bestPrev = best_;
-                Scalar bestvalPrev = funcvalues_(best_);
-                evaluateObjective();
-
-                // check if there was any improvement
-                if(funcvalues_(best_) < bestvalPrev)
-                {
-                    xdiff = (particles_.col(best) - particlesPrev_.col(bestPrev)).norm();
-                    fdiff = std::abs(funcvalues_(best_) - bestvalPrev);
-                }
-
-                if(verbose_)
-                {
-                    std::cout << "it=" << it
-                        << "\tfdiff=" << fdiff
-                        << "\txdiff" << xdiff
-                        << "\tf=" << funcvalues_(best_)
-                        << "\tx=" << particles_.col(best_).transpose()
-                        << std::endl;
-                }
-
-                ++it;
+                if(bounds(0, i) >= bounds(1, i))
+                    throw std::runtime_error("bounds min is greater than max");
             }
+
+            Matrix currParticles = particles;
+
+            return _minimize(objective, bounds, currParticles);
         }
 
     };
