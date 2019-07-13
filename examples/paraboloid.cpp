3589651366d9a24bb39802433b0fb04c494793c5
Fabian Meyer <user8324@posteo.net>
2019-07-13 12:09:29 +0530

    Add an example

--- examples/paraboloid.cpp
+++ examples/paraboloid.cpp
@@ -1,44 +1,3 @@
-# pso-cpp
-
-![Cpp11](https://img.shields.io/badge/C%2B%2B-11-blue.svg)
-![License](https://img.shields.io/packagist/l/doctrine/orm.svg)
-![Travis Status](https://travis-ci.org/Rookfighter/pso-cpp.svg?branch=master)
-![Appveyer Status](https://ci.appveyor.com/api/projects/status/cl5iljq9bq6lcusu?svg=true)
-
-pso-cpp is a header-only C++ library for particle swarm optimization using
-the Eigen3 linear algebra library.
-
-## Install
-
-Simply copy the header file into your project or install it using
-the CMake build system by typing
-
-```bash
-cd path/to/repo
-mkdir build
-cd build
-cmake ..
-make install
-```
-
-The library requires Eigen3 to be installed on your system.
-In Debian based systems you can simply type
-
-```bash
-apt-get install libeigen3-dev
-```
-
-Make sure Eigen3 can be found by your build system.
-
-## Usage
-
-There are three steps to use gradient-descent-cpp:
-
-* Implement your objective function as functor
-* Instantiate the particle swarm optimizer
-* Choose your parameters
-
-```cpp
 #include <psocpp.h>
 
 // Implement an objective functor.
@@ -100,4 +59,3 @@ int main()
 
     return 0;
 }
-```
